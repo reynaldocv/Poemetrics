30f82a9a92be92ccf43d35f97af9e70c6a920261
reynaldocv <reynaldocv@gmail.com>
2026-07-17 00:15:12 -0300

    123

--- Poemetrics/settings.py
+++ Poemetrics/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/home/kai/Documents/PoetricsEnv/Poemetrics/Poemetrics/static'
+STATIC_ROOT = '/home/reynaldocv/poemetrics/Poemetrics/static'
 #STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATICFILES=[STATIC_ROOT]
 
